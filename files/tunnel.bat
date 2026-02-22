2e40b91b480ac6a54600ebc2a514adef292da362
test6744 <234492490+test6744@users.noreply.github.com>
2026-02-22 10:58:00 +0800

    Log connection output to connection.txt
    Redirect output of pinggy-win-x64.exe to connection.txt

--- files/tunnel.bat
+++ files/tunnel.bat
@@ -1,6 +1,6 @@
 @echo off
 curl -L https://github.com/Pinggy-io/cli-js/releases/download/v0.3.5/pinggy-win-x64.exe --output pinggy-win-x64.exe
 :start
-pinggy-win-x64.exe -p 443 -R0:127.0.0.1:3389 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 tcp@ap.free.pinggy.io
+pinggy-win-x64.exe -p 443 -R0:127.0.0.1:3389 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 tcp@ap.free.pinggy.io > connection.txt
 timeout /t 10
 goto start
